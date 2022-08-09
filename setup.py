f89ffe295af3366380441d6811857b2065081843
Ganesh Dagadi <73548263+ganesh-dagadi@users.noreply.github.com>
2022-08-09 13:44:02 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import os
 def setup():
     CURR_DIR = os.path.dirname(os.path.realpath(__file__))
     base_path = chr(92).join(CURR_DIR.split(chr(92), 3)[:3])
-    mod_dir = base_path + '\mod_manager\modules'
+    mod_dir = base_path + '\gdmm\modules'
     sys.path.append(mod_dir)
     load_dotenv()
